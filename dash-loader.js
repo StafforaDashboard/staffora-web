62d84713f87dc3953f0249b3536be71b4684d3bb
Kimon <u0785621364@gmail.com>
2026-08-31 00:52:28 +0200

    App assets a0-a3 + loader v25

--- dash-loader.js
+++ dash-loader.js
@@ -1,13 +1,12 @@
 (function(){
-  var n=9;
+  var n=11;
   Promise.all(Array.from({length:n},function(_,i){
-    return fetch('c'+i+'.txt?v=22').then(function(r){
-      if(!r.ok) throw new Error('c'+i+' '+r.status);
+    return fetch('a'+i+'.txt?v=25').then(function(r){
+      if(!r.ok) throw new Error('a'+i+' '+r.status);
       return r.text();
     });
   })).then(function(parts){
-    var b64=parts.join('');
-    var bin=atob(b64);
+    var bin=atob(parts.join(''));
     var bytes=new Uint8Array(bin.length);
     for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
     return new Response(bytes).arrayBuffer().then(function(buf){
@@ -18,6 +17,6 @@
     s.textContent=new TextDecoder().decode(buf);
     document.body.appendChild(s);
   }).catch(function(e){
-    document.body.insertAdjacentHTML('beforeend','<div style="padding:24px;color:#fca5a5;font-family:system-ui">'+String(e.message||e)+'</div>');
+    document.body.insertAdjacentHTML('beforeend','<div style="padding:24px;color:#fca5a5;font-family:system-ui">'+e.message+'</div>');
   });
 })();
